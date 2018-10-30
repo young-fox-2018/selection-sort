577fcc11070c29066bb3d50f3ea2afe80b4abcdc
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 17:04:24 +0700

    kumpul

--- selection_sort.js
+++ selection_sort.js
@@ -1,35 +1,44 @@
 function selectionSort(input){
-  // KEDUA DARI ERAKHIR G BISA
-  // var result = []
-  // var tampung = 99999
 
-  // for(var i = 0 ; i < input.length ; i++){
-  //   if(input[i] < tampung){
-  //     result.unshift(input[i])
-  //     tampung = input[i]
-  //   }else{
-  //     result.push(input[i])
-  //   }
-  // }
-  // return result
+  for(var i = 0; i < input.length; i++){
+    var initial = input[i];
+    var temp = input[i+1] ;
+    var indextemp = i+1;
 
-  for(var i = 0 ; i < input.length ; i++){
-    var tampung = 999999;
-    var index ;
-
-    for(var j = i ; j < input.length ; j++){
-      if(input[j] < tampung){
-        tampung = input[j];
-        index = j
+    for(var j = i+1; j < input.length; j++){
+      if(temp > input[j]){
+          temp = input[j]
+          indextemp = j   
       }
     }
+    // console.log(temp)
+    if(initial > temp){
+        input[i] = temp
+        input[indextemp] = initial
+    }
+}
+return input
 
-    var temp = input[i];
-    input[i] = input[index]
-    input[index] = temp
-  }
-  return input
+
+
 }
 
 console.log(selectionSort([33, 2 ,52 , 106 , 73 ]));
 console.log(selectionSort([13, 5 , 22 , 99 , 11 ]));
+  // for(var i = 0 ; i < input.length ; i++){
+  //   tampung = input[i];
+  //   var index ;
+  
+  //   for(var j = i ; j < input.length ; j++){
+      
+  //     if(input[j] < tampung){
+  //       tampung = input[j];
+  //       index = j
+  //     }
+  //   }
+  
+  //   var temp = input[i];
+  //   input[i] = input[index]
+  //   input[index] = temp
+  // }
+  // return input
