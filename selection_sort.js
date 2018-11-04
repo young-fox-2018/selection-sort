cc5281f725a58d1bb09bf5500ff1b48e7b7961c9
fauzia <fauziaazia171097@gmail.com>
2018-11-04 18:23:33 +0700

    fixed bug

--- selection_sort.js
+++ selection_sort.js
@@ -1,24 +1,23 @@
-function selectingSort(input){
-    for ( var i = 0 ; i < input.length ; i++ ) {
-        var terkecil = input[i];
-        var index ;
-    
-           for ( var j = i ; j < input.length ; j++ ) {
-             if( input[j] < terkecil ) {
-               terkecil = input[j]
-               index = j
-             }
-           }
-       
-        var temp  =  input[i]
-    
-        input[i]  =  input[index]
-    
-        input[index] = temp
-       }
+function selectingSort(input) { 
+        let num = input.length; 
+  
+         
+        for (let i = 0; i < num-1; i++) { 
+            
+            let minIndex = i; 
+            for (let j = i+1 ; j < num ; j++) {
 
-     return input
-}
+              if (input[j] < input[minIndex]) 
+                  minIndex = j; 
+
+            }
+            
+            let temp = input[minIndex]; 
+            input[minIndex] = input[i]; 
+            input[i] = temp; 
+        } 
+        return input
+    } 
 
 console.log(selectingSort([33,2,52,106,73]))
 console.log(selectingSort([13,5,22,99,11]))
